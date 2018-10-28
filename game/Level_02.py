f411c68a1c84dc056f6b714795f0ef2dcc9e20b2
Anthony Bilic <anthonybilic@Anthonys-MacBook-Pro.local>
2018-10-28 10:21:57 -0700

    Game template up, working to get it working with pycharm properly

---
+++ game/Level_02.py
@@ -0,0 +1,37 @@
+# Create platforms for the level
+class Level_02(Level):
+    """ Definition for level 2. """
+
+    def __init__(self, player):
+        """ Create level 1. """
+
+        # Call the parent constructor
+        Level.__init__(self, player)
+
+        self.level_limit = -1000
+
+        # Array with type of platform, and x, y location of the platform.
+        level = [[210, 70, 500, 550],
+                 [210, 70, 800, 400],
+                 [210, 70, 1000, 500],
+                 [210, 70, 1120, 280],
+                 ]
+
+        # Go through the array above and add platforms
+        for platform in level:
+            block = Platform(platform[0], platform[1])
+            block.rect.x = platform[2]
+            block.rect.y = platform[3]
+            block.player = self.player
+            self.platform_list.add(block)
+
+        # Add a custom moving platform
+        block = MovingPlatform(70, 70)
+        block.rect.x = 1500
+        block.rect.y = 300
+        block.boundary_top = 100
+        block.boundary_bottom = 550
+        block.change_y = -1
+        block.player = self.player
+        block.level = self
+        self.platform_list.add(block)
