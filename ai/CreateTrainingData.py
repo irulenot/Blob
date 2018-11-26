93ad6e677c2e2967bdc4f4acfd1d13e00e135aee
Anthony Bilic <anthony.i.bilic@gmail.com>
2018-11-25 21:47:33 -0800

    Adding ai if I missed them

--- ai/CreateTrainingData.py
+++ ai/CreateTrainingData.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def getTrainingDataframe():
     data = pd.read_csv(Constants.PLAY_DATA_PATH, sep="\n", header=None)
-    # parsed_data = data[data[:].str.contains("KeyDown")]
-    print(data)
+    parsed_data = data[data[:].str.contains("KeyDown")]
+    print(parsed_data)
 
 getTrainingDataframe()
